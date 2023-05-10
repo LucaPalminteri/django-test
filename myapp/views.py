0fe002d850cea0a39303a8b01296964653465bf1
LucaPalminteri <lucapalminteri02@gmail.com>
2023-05-10 19:10:06 -0300

    replace html with a file and start planning integration with ML models

--- myapp/views.py
+++ myapp/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse('<h1>Home</h1><a href="http://127.0.0.1:8000/about">Go to About</a>')
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse('<h1>About</h1><a href="http://127.0.0.1:8000/">Go to Home</a>')
+
+# def result(request):
+#     user_input_age = int(request.GET["age"])
+#     prediction = fake_model.fake_predict(user_input_age)
+#     return render(request, 'result.html', {'prediction':prediction})
